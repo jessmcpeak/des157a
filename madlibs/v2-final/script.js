06cd717333a21cb15da20ae50856c75a4e9a8e81
jlmcpeak <you@youremail.com>
2025-02-10 19:29:43 -0800

    js progress

--- madlibs/v2-final/script.js
+++ madlibs/v2-final/script.js
@@ -2,19 +2,24 @@
     `use strict`;
     console.log(`Reading JS`);
 
+
+    // FORM DATA TO MADLIB
+
+
     // DOM reference variables
-    const myForm = document.querySelector(`form`);
+    const myForm = document.querySelector(`form input[type="submit"]`);
     const madlib = document.querySelector(`#madlib`);
     const formData = document.querySelectorAll(`input[type=text]`);
 
     // event handler that kicks everything off
-    myForm.addEventListener(`submit`, function(event){
+    myForm.addEventListener(`click`, function(event){
         event.preventDefault();
+        
         console.log(`form submitted`);
         processFormData(formData);
-    });
 
-    document.querySelectorAll(`input[type="submit"]`);
+        document.querySelector(`#sitePageResults`).classList.remove(`displayNone`);
+    });
 
     // taking the guts of the programming and putting them into a function - takes the data from the form as an input and extracts the data from it
     function processFormData(formData){
@@ -50,7 +55,7 @@
         const myText = `
             <h3>"My Fellow <b>${words[0]}</b>...</h3>
 
-            <p>This nation used to be the <b>${words[1]}</b> nation on Earth. But in these past <b>${words[2]}</b> years, the previous administration has done a <b>${words[3]}</b> job handling the <b>${words[4]}</b> situation, and they've allowed for far-<b>${words[5]}</b> radicals like <b>${words[6]}</b> to shape the country. Well, not anymore.</p>
+            <p>This nation used to be the <b>${words[1]}</b> nation on Earth! But in these past <b>${words[2]}</b> years, the previous administration has done a <b>${words[3]}</b> job handling the <b>${words[4]}</b> situation, and they've allowed for far-<b>${words[5]}</b> radicals like <b>${words[6]}</b> to shape the country. Well, not anymore.</p>
             <p>With my new administration, we will see a return to laws based on real values, the values that <b>${words[7]}</b> teaches us in <b>${words[8]}</b>. We are going to do something about the <b>${words[9]}</b> who are taking our <b>${words[10]}</b>. We are going to protect our nation's <b>${words[11]}</b>. And finally, we are going to make sure our <b>${words[12]}</b> learn that there are only <b>${words[13]}</b> genders!</p>
             <p>We are going to make <b>${words[14]}</b> <b>${words[15]}</b> again!"</p>`; // access the words from the array in the madlib where you want them to show up
 
@@ -61,4 +66,50 @@
             eachField.value = ``;
         }
     }
+
+
+    // FORM PAGINATION
+
+
+    document.querySelector(`#c1`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage1`).classList.add(`displayNone`);
+        document.querySelector(`#formPage2`).classList.remove(`displayNone`);
+    });
+
+    document.querySelector(`#gb1`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage2`).classList.add(`displayNone`);
+        document.querySelector(`#formPage1`).classList.remove(`displayNone`);
+    });
+
+    document.querySelector(`#c2`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage2`).classList.add(`displayNone`);
+        document.querySelector(`#formPage3`).classList.remove(`displayNone`);
+    });
+
+    document.querySelector(`#gb2`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage3`).classList.add(`displayNone`);
+        document.querySelector(`#formPage2`).classList.remove(`displayNone`);
+    });
+
+    document.querySelector(`#c3`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage3`).classList.add(`displayNone`);
+        document.querySelector(`#formPage4`).classList.remove(`displayNone`);
+    });
+
+    document.querySelector(`#gb3`).addEventListener(`click`, function(event){
+        event.preventDefault();
+
+        document.querySelector(`#formPage4`).classList.add(`displayNone`);
+        document.querySelector(`#formPage3`).classList.remove(`displayNone`);
+    });
 })();
