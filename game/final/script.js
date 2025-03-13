ec18e4809828bd49474ece890045a8f34ded95a6
jlmcpeak <you@youremail.com>
2025-03-13 01:24:38 -0700

    portal link

--- game/final/script.js
+++ game/final/script.js
@@ -4,39 +4,29 @@
 
     for(let option of document.querySelectorAll(`.bird-select`)){
         option.addEventListener(`click`, function(event){
-            event.preventDefault();
-
             option.setAttribute(`class`, `bird-select selected`);
         });
     }
 
     document.querySelector(`#choose-species`).addEventListener(`click`, function(event){
-        event.preventDefault();
-
         document.querySelector(`#select`).setAttribute(`class`, `screen hidden`);
         document.querySelector(`#game`).setAttribute(`class`, `screen`);
         document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar timer-animation`);
     });
 
     document.querySelector(`#timer-progress`).addEventListener(`animationend`, function(event){
-        event.preventDefault();
-
         document.querySelector(`#game`).setAttribute(`class`, `screen hidden`);
         document.querySelector(`#lose`).setAttribute(`class`, `screen`);
         document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar`);
     });
 
     document.querySelector(`#try-again`).addEventListener(`click`, function(event){
-        event.preventDefault();
-
         document.querySelector(`#lose`).setAttribute(`class`, `screen hidden`);
         document.querySelector(`#game`).setAttribute(`class`, `screen`);
         document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar timer-animation`);
     });
 
     document.querySelector(`#dance`).addEventListener(`click`, function(event){
-        event.preventDefault();
-
         document.querySelector(`#dance`).style.width = `${document.querySelector(`#dance`).style.width + 10}px`;
     });
 })();
