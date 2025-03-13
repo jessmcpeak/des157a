3af24a2a3c9afdd8874251d6cc45653dcefa552b
jlmcpeak <you@youremail.com>
2025-03-13 02:33:01 -0700

    basic functionality

--- game/final/script.js
+++ game/final/script.js
@@ -2,13 +2,17 @@
     `use strict`;
     console.log(`running js`);
 
-    for(let option of document.querySelectorAll(`.bird-select`)){
+    let loveProgress = 5;
+
+    for (const option of document.querySelectorAll(`.bird-select`)) {
         option.addEventListener(`click`, function(event){
             option.setAttribute(`class`, `bird-select selected`);
         });
     }
 
     document.querySelector(`#choose-species`).addEventListener(`click`, function(event){
+        loveProgress = 5;
+        document.querySelector(`#love-progress`).style.width = `calc(${loveProgress}% + 4px)`;
         document.querySelector(`#select`).setAttribute(`class`, `screen hidden`);
         document.querySelector(`#game`).setAttribute(`class`, `screen`);
         document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar timer-animation`);
@@ -21,12 +25,36 @@
     });
 
     document.querySelector(`#try-again`).addEventListener(`click`, function(event){
+        loveProgress = 5;
+        document.querySelector(`#love-progress`).style.width = `calc(${loveProgress}% + 4px)`;
         document.querySelector(`#lose`).setAttribute(`class`, `screen hidden`);
         document.querySelector(`#game`).setAttribute(`class`, `screen`);
         document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar timer-animation`);
     });
 
     document.querySelector(`#dance`).addEventListener(`click`, function(event){
-        document.querySelector(`#dance`).style.width = `${document.querySelector(`#dance`).style.width + 10}px`;
+        if (loveProgress === 95) {
+            document.querySelector(`#love-progress`).addEventListener(`transitionend`, function(event){
+                document.querySelector(`#game`).setAttribute(`class`, `screen hidden`);
+                document.querySelector(`#win`).setAttribute(`class`, `screen`);
+                document.querySelector(`#timer-progress`).setAttribute(`class`, `progress-bar`);
+            });
+            loveProgress += 5;
+            document.querySelector(`#love-progress`).style.width = `calc(${loveProgress}% + 4px)`;
+        } else if (loveProgress < 95) {
+            loveProgress += 5;
+            document.querySelector(`#love-progress`).style.width = `calc(${loveProgress}% + 4px)`;
+        }
     });
+
+    document.querySelector(`#learn-more`).addEventListener(`click`, function(event){
+        document.querySelector(`#win`).setAttribute(`class`, `screen hidden`);
+        document.querySelector(`#info`).setAttribute(`class`, `screen`);
+    });
+
+    for (const button of document.querySelectorAll(`.choose-another-bird`)) {
+        button.addEventListener(`click`, function(event){
+            location.reload();
+        });
+    }
 })();
